717f81ad96a758b7ef87ca3cc060cfd22fe3e79a
StarGrim33 <prosky33@gmail.com>
2023-08-04 15:38:17 +0300

    Cleaned code

--- Stellar Hero The Battle for Earth/Assets/Scripts/Interfaces/IControllable.cs
+++ Stellar Hero The Battle for Earth/Assets/Scripts/Interfaces/IControllable.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 
 public interface IControllable
 {
+<<<<<<< Updated upstream
 
     public void Move();
 
     public void TryDash();
 
     public void SetDirection(Vector2 direction);
+=======
+    void Move();
+
+    void TryDash();
+
+    void SetDirection(Vector2 direction);
+>>>>>>> Stashed changes
 }
