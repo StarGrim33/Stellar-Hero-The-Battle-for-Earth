23e2fa8f93474af3ad128a77ccda7286e9f09ace
StarGrim33 <prosky33@gmail.com>
2023-08-09 14:26:23 +0300

    added logic for crosshair

--- Stellar Hero The Battle for Earth/Assets/Scripts/Weapon/Weapon.cs
+++ Stellar Hero The Battle for Earth/Assets/Scripts/Weapon/Weapon.cs
@@ -12,23 +12,70 @@ public class Weapon : MonoBehaviour, IWeapon
     [Space, Header("Bullet")]
     [SerializeField] private PoolObjectSpawnComponent _spawnComponent;
     [SerializeField] private float _force = 10;
+    [SerializeField] private GameObject _crosshair;
 
     public Vector3 Target { get; private set; }
 
     private List<GameObject> _enemies;
     private Vector2 _directionToTarget;
+    private GameObject _currentTarget;
+
+    private void Update()
+    {
+        // Обновляем список врагов
+        _enemies = _enemyChecker.Check();
+
+        // Если нет текущей цели или она мертва, ищем ближайшего живого врага
+        if (_currentTarget == null || _currentTarget.GetComponent<EnemyHealth>().CurrentHealth <= 0)
+        {
+            _currentTarget = FindClosestLivingEnemy();
+        }
+
+        // Обновляем позицию прицела по текущей цели
+        if (_currentTarget != null)
+        {
+            UpdateCrossHairPosition(_currentTarget.transform.position);
+        }
+
+        // Если есть живая цель и прошла задержка, стреляем
+        if (_currentTarget != null && _shotCooldown.IsReady())
+        {
+            RotateToTarget(_currentTarget.transform.position);
+            _shotCooldown.Reset();
+            SpawnBullet();
+        }
+    }
+
+    private GameObject FindClosestLivingEnemy()
+    {
+        GameObject closestEnemy = null;
+        float closestDistance = float.MaxValue;
+
+        foreach (var enemy in _enemies)
+        {
+            if (enemy.GetComponent<EnemyHealth>().CurrentHealth > 0)
+            {
+                float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+                if (distanceToEnemy < closestDistance)
+                {
+                    closestDistance = distanceToEnemy;
+                    closestEnemy = enemy;
+                }
+            }
+        }
+
+        return closestEnemy;
+    }
 
     public void PerformShot()
     {
-        _enemies = _enemyChecker.Check();
 
         if (_enemies != null && _enemies.Count > 0 && _shotCooldown.IsReady())
         {
+
             if (_enemies.Count > 0)
             {
-                Target = _enemies[0].transform.position;
-
-                RotateToTarget(Target);
+                RotateToTarget(_currentTarget.transform.position);
             }
 
             _shotCooldown.Reset();
@@ -41,7 +88,7 @@ public class Weapon : MonoBehaviour, IWeapon
     {
         GameObject gameObject = _spawnComponent.Spawn();
 
-        if(gameObject.TryGetComponent(out Bullet bullet))
+        if (gameObject.TryGetComponent(out Bullet bullet))
         {
             bullet.gameObject.SetActive(true);
             bullet.AddForce(_directionToTarget, _force);
@@ -53,5 +100,11 @@ public class Weapon : MonoBehaviour, IWeapon
         _directionToTarget = target - transform.position;
         float angle = Mathf.Atan2(_directionToTarget.y, _directionToTarget.x) * Mathf.Rad2Deg + 90f;
         transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+
+    private void UpdateCrossHairPosition(Vector3 vector)
+    {
+        _crosshair.transform.position = new Vector2(vector.x, vector.y);
+        Debug.Log($"Crosshair position: {_crosshair.transform.position}, Target position: {vector}");
     }
 }
