fb50987f4d513b0b3433a4b681c04c982394d374
Владислав <wwwxumuk@mail.ru>
2023-08-25 15:04:29 +0300

    Mine Grafics

--- Stellar Hero The Battle for Earth/Assets/Scripts/Bullets/BulletTypes/BulletTypes.cs
+++ Stellar Hero The Battle for Earth/Assets/Scripts/Bullets/BulletTypes/BulletTypes.cs
@@ -3,5 +3,5 @@ public enum BulletType
     none,
     pierce,
     rebound,
-    chain
+    explosive
 }
--- Stellar Hero The Battle for Earth/Assets/Scripts/Bullets/BulletTypes/Mine.cs
+++ Stellar Hero The Battle for Earth/Assets/Scripts/Bullets/BulletTypes/Mine.cs
@@ -1,11 +1,26 @@
+using Assets.Scripts.Components.Checkers;
 using UnityEngine;
 
 public class Mine : Bullet
 {
+    [SerializeField] private CheckCircleOverlap _exploseArea;
+
     public override void Shot(Vector3 startPoint, Vector3 targetPoint, float modifySpeed, int modifyDamage, BulletType type = BulletType.none, int typePower =0)
     {
         transform.position = startPoint;
 
         SetDamage(modifyDamage);
+    }
+
+    private void Explose()
+    {
+        var targets = _exploseArea.Check();
+        foreach (var target in targets)
+        {
+            if(target.TryGetComponent<EnemyHealth>(out EnemyHealth enemyHealth))
+            {
+                enemyHealth.TakeDamage(_damage);
+            }
+        }
     }
 }
