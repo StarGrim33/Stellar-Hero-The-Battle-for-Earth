6a7121c2c9397e8a72381afdc953ae78e9dd69e3
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2023-09-13 14:10:16 +0300

    added defeat panel and mediator

--- Stellar Hero The Battle for Earth/Assets/Scripts/Player/LevelSystem/Bootstrap.cs
+++ Stellar Hero The Battle for Earth/Assets/Scripts/Player/LevelSystem/Bootstrap.cs
@@ -5,12 +5,18 @@ public class Bootstrap : MonoBehaviour
     [SerializeField] private LevelWindow _levelWindow;
     [SerializeField] private PlayerAnimator _playerAnimator;
     [SerializeField] private ExperienceHandler _experienceHandler;
+    [SerializeField] private GameplayMediator _gameplayMediator;
+    [SerializeField] private DefeatPanel _defeatPanel;
+    [SerializeField] private PlayerHealth _playerHealth;
 
     private void Awake()
     {
-        PlayerLevelSystem playerLevelSystem = new PlayerLevelSystem();
+        PlayerLevelSystem playerLevelSystem = new();
+        Level level = new(_playerHealth);
         _levelWindow.SetLevelSystem(playerLevelSystem);
         _playerAnimator.SetLevelSystem(playerLevelSystem);
         _experienceHandler.Init(playerLevelSystem);
+        _gameplayMediator.Initialize(level);
+        _defeatPanel.Initialize(_gameplayMediator);
     }
 }
